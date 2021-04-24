155ba90444b44ec78dd1da9eb64f11c864a70209
Mugi <muugii1222@yahoo.com>
2021-04-24 15:52:26 -0500

    added more work

--- src/day1/HelloWorld.java
+++ src/day1/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Adding new GitHub");
+        System.out.println("stuff are on github");
     }
 
 }

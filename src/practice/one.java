f7c360b85b4d8cfdee510e04a25b1a7c37855471
Mugi <muugii1222@yahoo.com>
2021-05-01 21:25:44 -0500

    added line code

--- src/practice/one.java
+++ src/practice/one.java
@@ -4,5 +4,6 @@ public class one {
     public static void main(String[] args) {
         System.out.println("Let's do some practice");
         System.out.println("Added one more line");
+        System.out.println("added second");
     }
 }
